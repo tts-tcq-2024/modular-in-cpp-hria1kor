c830ef3d42b753e023aa0e319b2ce424eedb4259
hria1kor <hrithikranjan9304@gmail.com>
2024-07-29 16:55:09 +0530

    Create colour.h

---
+++ colour.h
@@ -0,0 +1,32 @@
+// colorcoder.h
+#ifndef COLORCODER_H
+#define COLORCODER_H
+
+#include <string>
+
+namespace TelCoColorCoder {
+    enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+    enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
+
+    extern const char* MajorColorNames[];
+    extern const char* MinorColorNames[];
+    extern int numberOfMajorColors;
+    extern int numberOfMinorColors;
+
+    class ColorPair {
+    private:
+        MajorColor majorColor;
+        MinorColor minorColor;
+    public:
+        ColorPair(MajorColor major, MinorColor minor);
+        MajorColor getMajor();
+        MinorColor getMinor();
+        std::string ToString();
+    };
+
+    ColorPair GetColorFromPairNumber(int pairNumber);
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+    std::string GetReferenceManual();
+}
+
+#endif // COLORCODER_H
